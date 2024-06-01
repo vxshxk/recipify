65bc65b0f8e6ad432ea87935ea57680cc4d48a7d
ady <adityapandia18@gmail.com>
2024-06-01 22:38:11 +0530

    Add basic image upload support

--- recipify/recipify/views.py
+++ recipify/recipify/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse, redirect
-from .forms import SignupForm, LoginForm
+from .forms import SignupForm, LoginForm, UploadFileForm
+from .models import FoodImage
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            form = UploadFileForm()
+    
+    images = FoodImage.objects.all()
+    form = UploadFileForm()
+    context = {
+        'form': form,
+        'images': images,
+    }
+    return render(request, 'index.html', context)
+
+@login_required
+def gallery(request):
+    images = FoodImage.objects.all()
+    context = {
+        'images': images,
+    }
+    return render(request, 'gallery.html', context)
 
 
 def loginPage(request):
