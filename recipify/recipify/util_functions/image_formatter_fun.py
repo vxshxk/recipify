65670fa861e6cb7466c271fdaca18f551ebc13ee
ady <adityapandia18@gmail.com>
2024-06-07 00:37:09 +0530

    add recipify to import headings

--- recipify/recipify/util_functions/image_formatter_fun.py
+++ recipify/recipify/util_functions/image_formatter_fun.py
@@ -38,19 +38,3 @@ def imageFormatter(img):
     
     return proc
 
-
-image_path = 'examples/aaa.jpg'
-img = Image.open(image_path)
-img_canny = imageFormatter(img)
-
-fig, ax = plt.subplots(1, 2, figsize=(12, 6))
-ax[0].imshow(cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB))
-ax[0].set_title('Original Image')
-ax[0].axis('off')
-
-ax[1].imshow(img_canny, cmap='gray')
-ax[1].set_title('Sharper image')
-ax[1].axis('off')
-
-plt.show()
-
--- recipify/recipify/utils.py
+++ recipify/recipify/utils.py
@@ -1,5 +1,5 @@
-from util_functions.image_formatter_fun import imageFormatter
-from util_functions.gemini_call_fun import getRecipes
+from recipify.util_functions.image_formatter_fun import imageFormatter
+from recipify.util_functions.gemini_call_fun import getRecipes
 
 import cv2
 import matplotlib.pyplot as plt
@@ -10,19 +10,4 @@ def image_formatter(img):
 
 def getRecipeList(ingredients):
     return getRecipes(ingredients)
-
-image_path = 'examples/aaa.jpg'
-img = Image.open(image_path)
-img_canny = image_formatter(img)
-
-fig, ax = plt.subplots(1, 2, figsize=(12, 6))
-ax[0].imshow(cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB))
-ax[0].set_title('Original Image')
-ax[0].axis('off')
-
-ax[1].imshow(img_canny, cmap='gray')
-ax[1].set_title('Sharper image')
-ax[1].axis('off')
-
-plt.show()
 
