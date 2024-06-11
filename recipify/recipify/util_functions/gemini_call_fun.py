5d7d22d42a59601418613218cc5a1566314051cb
vxshxk <vishakkamath.221ec226@nitk.edu.in>
2024-06-12 00:13:28 +0530

    [add] exception handling in gemini api calls

--- recipify/recipify/util_functions/gemini_call_fun.py
+++ recipify/recipify/util_functions/gemini_call_fun.py
@@ -36,15 +36,25 @@ def getRecipes(ingredients):
     safety_settings=safety_settings,
     generation_config=generation_config,
   )
-
-  response = model.generate_content("Give me the list of dishes and step by step recipe of how I can make using the following items: "
-                                    +str(ingredients)
-                                    + " Give the output in the json of having one field list which is a list of Recipe = { dish_name: str , ingredients_required: numbered list, recipe: bullet list }  with only json part and without the text and quotes before or after")
   
-  start_index = response.text.find('{')
-  end_index = response.text.rfind('}') + 1
-  jsonresponse = json.loads(response.text[start_index:end_index])
-  
+  try:
+    response = model.generate_content("Give me the list of dishes and step by step recipe of how I can make using the following items: "
+                                      +str(ingredients)
+                                      + " Give the output in the json of having one field list which is a list of Recipe = { dish_name: str , ingredients_required: numbered list, recipe: bullet list }  with only json part and without the text and quotes before or after")
+    
+    start_index = response.text.find('{')
+    end_index = response.text.rfind('}') + 1
+    jsonresponse = json.loads(response.text[start_index:end_index])
+  except:
+    jsonresponse = {
+    "list": [
+      {
+        "dish_name": "Sorry, some unknown error occured!",
+        "ingredients_required": [" "],
+        "recipe": [" "]
+      }
+    ]
+  }
   return jsonresponse
 
 
@@ -86,15 +96,24 @@ def getNutrition(recipeName):
     safety_settings=safety_settings,
     generation_config=generation_config,
   )
-
-  response = model.generate_content("Can you give me the nutritional breakdown of "+ str(recipeName) + ". It should include all the nutrients available in the meal, and the ones that are not"
-                                    + " Give the output in the format as List<Recipe> where Recipe = { dish_name: str , nutreints_present: bullet list, nutreints_absent: bullet list }")
   
-  start_index = response.text.find('{')
-  end_index = response.text.rfind('}') + 1
-  jsonresponse = json.loads(response.text[start_index:end_index])
-  
+  try:
+    response = model.generate_content("Can you give me the nutritional breakdown of "+ str(recipeName) + ". It should include all the nutrients available in the meal, and the ones that are not"
+                                      + " Give the output in the format as List<Recipe> where Recipe = { dish_name: str , nutreints_present: bullet list, nutreints_absent: bullet list }") 
+    start_index = response.text.find('{')
+    end_index = response.text.rfind('}') + 1
+    jsonresponse = json.loads(response.text[start_index:end_index])
+  except:
+    jsonresponse = {
+    "list": [
+      {
+        "dish_name": "Sorry, some unknown error occured!",
+        "nutreints_present": [" "],
+        "nutreints_absent": [" "]
+      }
+    ]
+  }
   return jsonresponse
 
-#print(getRecipes(["tomato", "onion", "potato", "soyabean", "soya sauce", "salt", "pepper", "chilli powder", "ginger", "garlic", "coriander", "cumin", "turmeric", "garam masala", "oil"]))
-#print(getNutrition("Simple Potato & Onion Curry"))
+print(getRecipes(["tomato", "onion", "potato", "soyabean", "soya sauce", "salt", "pepper", "chilli powder", "ginger", "garlic", "coriander", "cumin", "turmeric", "garam masala", "oil"]))
+print(getNutrition("Simple Potato & Onion Curry"))
